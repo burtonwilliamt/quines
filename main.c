d5e034302c4ecb1212a60f115718eb4ba96a07e1
burtonwilliamt <burtonwilliamt@gmail.com>
2017-04-10 13:50:44 -0400

    Fixed issue with percent characters

--- main.c
+++ main.c
@@ -10,8 +10,6 @@ void quineify(char* string){
 			printf("\\n");
 		}else if(string[i] == '\t'){
 			printf("\\t");
-		}else if(string[i] == '%'){
-			printf("\\%%");
 		}else if(string[i] == '\\'){
 			printf("\\\\");
 		}else{
@@ -21,12 +19,12 @@ void quineify(char* string){
 }
 
 int main(int argc, char* argv[]){
-	char* x = "#include <stdio.h>\n#include <string.h>\n\nvoid quineify(char* string){\n\tint len = strlen(string);\n\tfor( int i = 0; i < len; ++i){\n\t\tif(string[i] == '\"'){\n\t\t\tprintf(\"\\\\\\\"\");\n\t\t}else if(string[i] == '\\n'){\n\t\t\tprintf(\"\\\\n\");\n\t\t}else if(string[i] == '\\t'){\n\t\t\tprintf(\"\\\\t\");\n\t\t}else if(string[i] == '\%'){\n\t\t\tprintf(\"\\\\\%\%\");\n\t\t}else if(string[i] == '\\\\'){\n\t\t\tprintf(\"\\\\\\\\\");\n\t\t}else{\n\t\t\tprintf(\"\%c\", string[i]);\n\t\t}\n\t}\n}\n\nint main(int argc, char* argv[]){\n\tchar* x = \"\";\n\tchar first[512];\n\tchar second[512];\n\tstrncpy(first, x, 461);\n\tfirst[461] = '\\0';\n\tstrncpy(second, x+461, 200);\n\tprintf(\"\%s\", first);\n\tquineify(x);\n\tprintf(\"\%s\", second);\n}";
+	char* x = "#include <stdio.h>\n#include <string.h>\n\nvoid quineify(char* string){\n\tint len = strlen(string);\n\tfor( int i = 0; i < len; ++i){\n\t\tif(string[i] == '\"'){\n\t\t\tprintf(\"\\\\\\\"\");\n\t\t}else if(string[i] == '\\n'){\n\t\t\tprintf(\"\\\\n\");\n\t\t}else if(string[i] == '\\t'){\n\t\t\tprintf(\"\\\\t\");\n\t\t}else if(string[i] == '\\\\'){\n\t\t\tprintf(\"\\\\\\\\\");\n\t\t}else{\n\t\t\tprintf(\"%c\", string[i]);\n\t\t}\n\t}\n}\n\nint main(int argc, char* argv[]){\n\tchar* x = \"\";\n\tchar first[512];\n\tchar second[512];\n\tstrncpy(first, x, 412);\n\tfirst[412] = '\\0';\n\tstrncpy(second, x+412, 200);\n\tprintf(\"%s\", first);\n\tquineify(x);\n\tprintf(\"%s\", second);\n}";
 	char first[512];
 	char second[512];
-	strncpy(first, x, 461);
-	first[461] = '\0';
-	strncpy(second, x+461, 200);
+	strncpy(first, x, 412);
+	first[412] = '\0';
+	strncpy(second, x+412, 200);
 	printf("%s", first);
 	quineify(x);
 	printf("%s", second);
